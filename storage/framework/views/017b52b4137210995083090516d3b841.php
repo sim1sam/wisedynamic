ddc30d7957dc9b3c6f1b0003b6f2feb256e7a9d6
sim1sam <sim1sam@github.com>
2025-10-04 21:06:43 +0600

    chore: clean up cached views and update services configuration
    Remove outdated cached view files and update services configuration to include ConfigMakeCommand. This cleanup improves system performance by removing unnecessary cached files and ensures the console command is properly registered in the service container.

--- storage/framework/views/017b52b4137210995083090516d3b841.php
+++
@@ -1,3 +0,0 @@
-<footer class="main-footer">
-    <?php echo $__env->yieldContent('footer'); ?>
-</footer><?php /**PATH F:\laragon\www\wisedynamic\vendor\jeroennoten\laravel-adminlte\src/../resources/views/partials/footer/footer.blade.php ENDPATH**/ ?>
